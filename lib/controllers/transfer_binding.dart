43b411e69b1ab6d99b714eb079a0e64e667b968a
emre durgunlu <emredurgunlu@gmail.com>
2022-02-15 08:13:51 +0300

    Delete lib directory

--- lib/controllers/transfer_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/instance_manager.dart';
-import 'transfer_money.dart';
-
-class TransferBinding implements Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<TransferMoney>(() => TransferMoney());
-  }
-}
--- lib/controllers/transfer_money.dart
+++
@@ -1,5 +0,0 @@
-import 'package:get/state_manager.dart';
-
-class TransferMoney extends GetxController {
-  final selectedMoney = "0".obs;
-}
